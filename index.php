989ef5b30b669777c9353bd84c3d8636e97e25e0
alex00231 <33204537+alex00231@users.noreply.github.com>
2018-12-10 16:36:16 +0500

    0.0.1

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+include_once "./templates/header.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style/css/headStyle.css">
+</head>
+<body>
+<?php 
+generation_head_menu($mysqli);
+?>
+</body>
+</html>
